bd2d294013f3d4f9f795c2ea4ea985c88e51422e
Jon Fairbanks <fairplay89@gmail.com>
2021-07-18 21:13:25 -0700

    Fix filenames

--- yo.js
+++ yo.js
@@ -2,11 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-  vus: 5000,
+  vus: 1000,
   duration: '60s',
 };
 
 export default function () {
-  http.get('https://kube.fairbanks.dev');
+  http.get('https://fbnks.io/kube');
   sleep(1);
 };
